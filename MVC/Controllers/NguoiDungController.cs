5c0cf36861df803f594362c8bb14bbba8472eb3e
Hoang Trung <trung.phan150041@vnuk.edu.vn>
2018-02-08 11:31:37 +0700

    Second commit - Controllers and Models Updated

---
+++ MVC/Controllers/NguoiDungController.cs
@@ -0,0 +1,23 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            IEnumerable<mvcNguoiDungModel> persList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Person").Result;
+            persList = response.Content.ReadAsAsync<IEnumerable<mvcNguoiDungModel>>().Result;
+            
+            return View(persList);
+        }
+    }
+}
---
+++ MVC/Models/mvcNguoiDungModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class mvcNguoiDungModel
+    {
+        public int IdNguoiDung { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public Nullable<int> Age { get; set; }
+        public string HoTen { get; set; }
+        public Nullable<System.DateTime> NgaySinh { get; set; }
+        public string sdt { get; set; }
+    }
+}
